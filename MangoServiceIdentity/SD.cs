fc35da1b7bf8c4a75730b247333c6e588ea59555
Youssouf <yssouare@gmail.com>
2023-03-13 13:39:02 +0100

    Add IdentityServer static class SD

---
+++ MangoServiceIdentity/SD.cs
@@ -0,0 +1,59 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangoServiceIdentity
+{
+    public static class SD
+    {
+        public const string admin = "admin";
+        public const string customer = "customer";
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope> {
+                new ApiScope("Mango", "Mango Server"),
+                new ApiScope(name: "Read", displayName : "Read your Data"),
+                new ApiScope(name : "Write", displayName: "Write your Data"),
+                new ApiScope(name : "Delete", displayName: "Delete your Data")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    ClientSecrets= { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes={ "read", "write","profile"}
+                },
+                new Client
+                {
+                    ClientId="mango",
+                    ClientSecrets= { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris={ "https://localhost:44378/signin-oidc" },
+                    PostLogoutRedirectUris={"https://localhost:44378/signout-callback-oidc" },
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "mango"
+                    }
+                },
+            };
+
+
+    }
+}
